57e516d14f7a93f3a52786cd98a55e7261b01f4d
hymced <hymced@gmail.com>
2023-11-04 22:47:21 +0100

    feat: datepicker

---
+++ src/app/posts/components/datepicker/datepicker.component.ts
@@ -0,0 +1,26 @@
+import {Component} from '@angular/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+
+/** @title Basic datepicker */
+@Component({
+  selector: 'app-component-datepicker',
+  templateUrl: './datepicker.component.html',
+  styleUrls: ['./datepicker.component.scss'],
+  standalone: true,
+  imports: [
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatDatepickerModule, 
+    MatNativeDateModule],
+})
+export class DatepickerComponent {
+
+  altTheme: boolean = false;
+  changeTheme() {
+    this.altTheme = !this.altTheme;
+  }
+
+}
